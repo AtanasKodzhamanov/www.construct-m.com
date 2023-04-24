5d35a6238a347159fc0528df827f7ed9d420cd37
Atanas <kodzhamanov@gmail.com>
2023-04-24 19:55:11 +0100

    fresh start

--- consultm/src/App.js
+++ consultm/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import TopHeader from './Components/TopHeader';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="app-body">
-      <TopHeader />
+      <Header />
     </div>
   );
 }
