c11ba4eafe9d1f8cc86a37f675cabd7184e5b1e2
Atanas <kodzhamanov@gmail.com>
2023-05-05 04:26:55 +0100

    change links on hover

--- consultm/src/App.js
+++ consultm/src/App.js
@@ -8,34 +8,9 @@ import Construction from './Components/Construction';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Home from './Components/Home';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+
 
 function App() {
-
-
-  const [activeLink, setActiveLink] = useState('/');
-  const [prevPath, setPrevPath] = useState('/');
-  const location = useLocation();
-
-  useEffect(() => {
-    setActiveLink(location.pathname);
-    setPrevPath(location.pathname);
-  }, [location.pathname]);
-
-  useEffect(() => {
-    const header = document.querySelector('.content');
-    if (prevPath !== location.pathname) {
-      header.classList.add('fade-out');
-      setTimeout(() => {
-        header.classList.remove('fade-out');
-        header.classList.add('fade-in');
-      }, 300);
-      setTimeout(() => {
-        header.classList.remove('fade-in');
-      }, 600);
-    }
-  }, [prevPath, location.pathname]);
 
   return (
     <div className="app-body">
