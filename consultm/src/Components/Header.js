01e0d6823d9c0e98aa5cb2cf5c80b5aaa3bfadea
Atanas <kodzhamanov@gmail.com>
2023-05-31 02:00:12 +0100

    hamburger menu

--- consultm/src/Components/Header.js
+++ consultm/src/Components/Header.js
@@ -2,24 +2,75 @@ import React from 'react';
 import './Header.css';
 import { Link, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
 
     const [activeLink, setActiveLink] = useState('/');
     const location = useLocation();
+    const [navOpen, setNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        let navStatus = !navOpen;
+        setNavOpen(navStatus);
+
+        // Get the sidenav element
+        let sidenav = document.getElementById('mySidenav');
+
+        if (navStatus) {
+            // Open the sidenav
+            sidenav.style.width = '60%';
+        } else {
+            // Close the sidenav
+            sidenav.style.width = '0';
+        }
+    };
 
     useEffect(() => {
         setActiveLink(location.pathname);
     }, [location.pathname]);
 
 
+
+
+
     return (
         <div className="header-container">
+
             <div className="title">
                 <Link to="/">
                     <h1>CONSTRUCT-M</h1>
                 </Link>
             </div>
+
+            <div id="hamburger-icon" onClick={toggleNav}>
+                <FontAwesomeIcon icon={faBars} size="lg" style={{ color: 'white' }} />
+            </div>
+
+            <div id="mySidenav" className="sidenav">
+                <div className="studies-section">
+                    <Link className={activeLink === '/studies' ? 'active' : 'inactive'} to="/studies#preliminary">
+                        <h3>STUDIES</h3>
+                    </Link>
+                </div>
+                <div className="design-section">
+                    <Link className={activeLink === '/design' ? 'active' : ''} to="/design#concept">
+                        <h3>DESIGN</h3>
+                    </Link>
+                </div>
+                <div className="construction-section">
+                    <Link className={activeLink === '/construction' ? 'active' : ''} to="/construction#management">
+                        <h3>CONSTRUCTION</h3>
+                    </Link>
+                </div>
+                <div className="about-section">
+                    <Link className={activeLink === '/about' ? 'active' : ''} to="/about#team">
+                        <h3>ABOUT</h3>
+                    </Link>
+                </div>
+            </div>
+
             <div className="nav">
                 <div className="studies-section">
                     <Link className={activeLink === '/studies' ? 'active' : 'inactive'} to="/studies#preliminary">
